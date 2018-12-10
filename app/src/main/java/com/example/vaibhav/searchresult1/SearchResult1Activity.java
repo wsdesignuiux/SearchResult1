527e47ec971a55bc0ae2e8a2ab1342c99c58498b
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-10 11:07:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/searchresult1/SearchResult1Activity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.searchresult1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResult1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result1);
+    }
+}
